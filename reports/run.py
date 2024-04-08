ce6be66d9bbf2895cd90710083d7bd535f6e1625
Cosmo Santoni <santonicosmo@gmail.com>
2024-04-08 16:39:31 +0100

    move things around for orderly

---
+++ reports/run.py
@@ -0,0 +1,5 @@
+from orderly.run import orderly_run
+
+path = "X:\\Cosmo\\projects\\toy-mint-emulator\\src"
+
+id1 = orderly_run("benchmark", root=path)
---
+++ start/main.py
@@ -0,0 +1,7 @@
+## This ensures that we have initialised an outpack report scheme (better word than scheme needed...)
+
+from outpack.init import outpack_init
+
+path = "X:\\Cosmo\\projects\\toy-mint-emulator"
+
+outpack_init(path)
