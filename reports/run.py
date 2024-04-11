bb200fdb357db91d39f1add6e84a081a310ded5d
Cosmo Santoni <santonicosmo@gmail.com>
2024-04-11 13:34:49 +0100

    rename and notes

--- reports/run.py
+++ reports/run.py
@@ -2,4 +2,7 @@ from orderly.run import orderly_run
 
 path = "X:\\Cosmo\\projects\\toy-mint-emulator\\src"
 
+id0 = orderly_run("data", root=path)
 id1 = orderly_run("benchmark", root=path)
+
+#python -u "x:\Cosmo\projects\toy-mint-emulator\reports\run.py"
