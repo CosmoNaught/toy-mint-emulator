c3574ffc986d1ca3f53bfbf5e392fa4bdd4b534e
Cosmo Santoni <santonicosmo@gmail.com>
2024-04-17 13:22:16 +0100

    Create utils.py

---
+++ src/benchmark/utils.py
@@ -0,0 +1,13 @@
+import sys
+
+def list_modules(snapshot, message):
+    
+    '''This functions works out which libraries are essential for the
+    execution of the benchmarking tool'''
+    
+    current_modules = set(sys.modules.keys())
+    new_modules = current_modules - snapshot
+    print(f"{message} (newly loaded):")
+    for module in sorted(new_modules):
+        print(module)
+    return current_modules
