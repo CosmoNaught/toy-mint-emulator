6af5e7413e2c32ca367f2f40869270cad733dd86
Cosmo Santoni <santonicosmo@gmail.com>
2024-04-18 13:59:06 +0100

    Update run.py

--- reports/run.py
+++ reports/run.py
@@ -4,5 +4,5 @@ path = "X:\\Cosmo\\projects\\toy-mint-emulator\\src"
 
 id0 = orderly_run("data", root=path)
 id1 = orderly_run("benchmark", root=path)
-
+id2 = orderly_run("post", root=path)
 #python -u "x:\Cosmo\projects\toy-mint-emulator\reports\run.py"
